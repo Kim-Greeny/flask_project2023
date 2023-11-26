d303b4b64d45b9ef2de7797c8c728e98e45fadc5
‎최지원(호크마교양대학 호크마교양대학) <won0805@ewhain.net>
2023-11-26 09:01:52 +0000

    week12-3

--- application.py
+++ application.py
@@ -117,6 +117,40 @@ def reg_item_submit_post():
     
     return render_template("submit_item_result.html", data=data, img_path="static/images/{}".format(image_file.filename))
 
+@application.route('/show_heart/<name>/', methods=['GET'])
+def show_heart(name):
+    my_heart=DB.get_heart_byname(session['id'], name)
+    return jsonify({'my_heart':my_heart})
 
+@application.route('/like/<name>/', methods=['POST'])
+def like(name):
+    my_heart=DB.update_heart(session['id'], 'Y', name)
+    return jsonify({'msg':'좋아요 완료!'})
+
+@application.route('/unlike/<name>/', methods=['POST'])
+def unlike(name):
+    my_heart=DB.update_heart(session['id'], 'N', name)
+    return jsonify({'msg':'안좋아요 완료!'})
+
+def get_heart_byname(self, uid, name):
+    hearts=self.db.child("heart").child(uid).get()
+    target_value=""
+    if hearts.val()==None:
+        return target_value
+    
+    for res in hearts.each():
+        key_value==res.key()
+        
+        if key_value==name:
+            target_value=res.val()
+    return target_value
+
+def update_heart(self, user_id, isHeart, item):
+    heart_info={
+        "interested":isHeart
+    }
+    self.db.child("heart").child(user_id).child(item).set(heart_info)
+    return True
+    
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
